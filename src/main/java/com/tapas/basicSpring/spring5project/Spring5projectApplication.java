913a3c4d5ab79f3ff5c8ae40edb9e8bbfeef9ddc
tapas.satpathy <tapas.satpathy@gmail.com>
2018-11-15 19:31:42 +0900

    1st Spring 5 JPA project

---
+++ src/main/java/com/tapas/basicSpring/spring5project/Spring5projectApplication.java
@@ -0,0 +1,12 @@
+package com.tapas.basicSpring.spring5project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5projectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5projectApplication.class, args);
+	}
+}
---
+++ src/main/java/com/tapas/basicSpring/spring5project/model/Book.java
@@ -0,0 +1,92 @@
+package com.tapas.basicSpring.spring5project.model;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String isbn;
+    private String title;
+
+    @ManyToMany
+    @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
+    inverseJoinColumns = @JoinColumn(name = "author_id"))
+    private Set<Author> authors = new HashSet<>();
+
+    public Book() {
+    }
+
+    public Book(Long id, String isbn, String title) {
+        this.id = id;
+        this.isbn = isbn;
+        this.title = title;
+    }
+
+    public Book(Long id, String isbn, String title, Set<Author> authors) {
+        this.id = id;
+        this.isbn = isbn;
+        this.title = title;
+        this.authors = authors;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Set<Author> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(Set<Author> authors) {
+        this.authors = authors;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(id, book.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", isbn='" + isbn + '\'' +
+                ", title='" + title + '\'' +
+                ", authors=" + authors +
+                '}';
+    }
+}
